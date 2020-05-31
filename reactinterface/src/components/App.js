8c34d2797b108ae3c8a9e73c450267638a854ecf
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-05-31 12:18:46 -0400

    Adding data and additional stuffs in reactinterface

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
-import AddAppointment from './AddAppointment';
-import ListAppointment from './ListAppointment';
-import SearchAppointment from './SearchAppointment';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 bg-white">
-          <div className="container">
-            <AddAppointment />
-            <ListAppointment />
-            <SearchAppointment />
+import AddAppointments from './AddAppointment';
+import SearchAppointments from './SearchAppointment';
+import ListAppointments from './ListAppointment';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Ray',
+      myAppointments: []
+    };
+  }
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        });
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments} />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
