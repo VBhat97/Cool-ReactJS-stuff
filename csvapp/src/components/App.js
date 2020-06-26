da1234b1ba6a2fad959b348376bafda72775446f
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-26 01:11:22 -0400

    Added TODO;s

--- csvapp/src/components/App.js
+++ csvapp/src/components/App.js
@@ -5,7 +5,10 @@ import Bodytags from './Bodytags';
 function App() {
   return (
     <div>
-      <Bodytags />
+      {/* TODO: Add Nice CSV App Header to include Navbar with Home, About and Add CSV */}
+      {/* TODO: Add Form content by reading the csv and other parameters whichever needed */}
+      {/* TODO: Add Footer with Copyright Components */}
+      {/* <Bodytags /> */}
     </div>
   );
 }
