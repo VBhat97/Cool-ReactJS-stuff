8ea378810185323c6369e471299d2d88e803d901
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-26 00:03:24 -0400

    Added Bodytags stuff

--- csvapp/src/components/App.js
+++ csvapp/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../css/App.css';
+import Bodytags from './Bodytags';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Bodytags />
     </div>
   );
 }
---
+++ csvapp/src/components/Bodytags.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Bodytags extends Component{
+    render(){
+        return(
+            <div>Body--tags</div>
+        )
+    }
+}
+
+export default Bodytags;
