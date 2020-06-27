fc841e3441b1d1ea2398c22a6512981b50387870
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-27 02:00:44 -0400

    Removed Warnings

--- csvapp/src/components/App.js
+++ csvapp/src/components/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Header />
+      {/* TODO: Add Conditional Rendering based on the header data.*/}
       {/* TODO: Add Form content by reading the csv and other parameters whichever needed */}
       {/* TODO: Add Footer with Copyright Components */}
       {/* <Bodytags /> */}
