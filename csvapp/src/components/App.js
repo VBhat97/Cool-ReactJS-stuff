7442bb9fbc2557b94e925d7b8476a9cc370d31b6
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-28 20:09:41 -0400

    Added Home content

--- csvapp/src/components/App.js
+++ csvapp/src/components/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../css/App.css';
 import Header from './Header'
+import HomeBody from './HomeBody'
 
 function App() {
   return (
     <div>
       <Header />
+      <HomeBody />
       {/* TODO: Add Conditional Rendering based on the header data.*/}
       {/* TODO: Add Form content by reading the csv and other parameters whichever needed */}
       {/* TODO: Add Footer with Copyright Components */}
-      {/* <Bodytags /> */}
     </div>
   );
 }
---
+++ csvapp/src/components/HomeBody.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HomeBody extends Component{
+    render(){
+        return(
+            <div>
+                Adding the home content
+            </div>
+        )
+    }
+}
+
+export default HomeBody;
