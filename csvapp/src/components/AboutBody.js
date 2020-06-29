f33aa5f1cd4edea8870680532f51acba40c699ea
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-28 20:23:46 -0400

    Added About content

---
+++ csvapp/src/components/AboutBody.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AboutBody extends Component{
+    render(){
+        return(
+            <div>
+                Adding the about content
+            </div>
+        )
+    }
+}
+
+export default AboutBody;
--- csvapp/src/components/App.js
+++ csvapp/src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import '../css/App.css';
 import Header from './Header'
 import HomeBody from './HomeBody'
+import AboutBody from './AboutBody'
 
 function App() {
   return (
     <div>
       <Header />
       <HomeBody />
+      <AboutBody />
+      <UploadForm />
       {/* TODO: Add Conditional Rendering based on the header data.*/}
       {/* TODO: Add Form content by reading the csv and other parameters whichever needed */}
       {/* TODO: Add Footer with Copyright Components */}
