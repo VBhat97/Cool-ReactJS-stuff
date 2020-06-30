2533d7c3f3f3e078f3bb65f8320a15d832e83c01
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-06-30 04:14:18 -0400

    Almost done with alternative rendering

--- csvapp/src/components/App.js
+++ csvapp/src/components/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/App.css';
 import Header from './Header'
 import HomeBody from './HomeBody'
 import AboutBody from './AboutBody'
+import UploadBody from './UploadBody';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <HomeBody />
-      <AboutBody />
-      <UploadForm />
-      {/* TODO: Add Conditional Rendering based on the header data.*/}
-      {/* TODO: Add Form content by reading the csv and other parameters whichever needed */}
-      {/* TODO: Add Footer with Copyright Components */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      showHideDemo1: false,
+      showHideDemo2: false,
+      showHideDemo3: false
+    };
+  }
+  render(){
+    const { showHideDemo1, showHideDemo2, showHideDemo3 } = this.state;
+    return (
+      <div>
+        <Header />
+        {showHideDemo1 && <HomeBody />}
+        {showHideDemo2 && <AboutBody />}
+        {showHideDemo3 && <UploadBody />}
+        {/* TODO: Add Conditional Rendering based on the header data.*/}
+        {/* TODO: Add Form content by reading the csv and other parameters whichever needed */}
+        {/* TODO: Add Footer with Copyright Components */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- csvapp/src/components/Header.js
+++ csvapp/src/components/Header.js
@@ -2,15 +2,43 @@ import React, {Component} from 'react';
 import { Nav, Navbar} from 'react-bootstrap';
 
 class Header extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+          name: "React",
+          showHideDemo1: false,
+          showHideDemo2: false,
+          showHideDemo3: false
+        };
+        this.hideComponent = this.hideComponent.bind(this);
+      }
+
+      hideComponent(name) {
+        console.log(name)
+        switch (name) {
+          case "showHideDemo1":
+            this.setState({ showHideDemo1: !this.state.showHideDemo1 });
+            break;
+          case "showHideDemo2":
+            this.setState({ showHideDemo2: !this.state.showHideDemo2 });
+            break;
+          case "showHideDemo3":
+            this.setState({ showHideDemo3: !this.state.showHideDemo3 });
+            break;
+          default :
+            console.log("Default")
+        }
+      }
     render(){
         return(
             <div>
             <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">CSVApp</Navbar.Brand>
             <Nav className="mr-auto">
-              <Nav.Link href="#home" onClick={props.home}>Home</Nav.Link>
-              <Nav.Link href="#features" onClick={props.about}>About</Nav.Link>
-              <Nav.Link href="#pricing" onClick={props.upload}>Upload</Nav.Link>
+              <Nav.Link onClick={() => this.hideComponent("showHideDemo1")}>Home</Nav.Link>
+              <Nav.Link onClick={() => this.hideComponent("showHideDemo2")}>About</Nav.Link>
+              <Nav.Link onClick={() => this.hideComponent("showHideDemo3")}>Upload</Nav.Link>
             </Nav>
           </Navbar>
           </div>
