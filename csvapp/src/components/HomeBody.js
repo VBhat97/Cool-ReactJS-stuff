1f84cde5f2293b8e93a2ef9d4213fc1018f192a7
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-07-02 00:31:59 -0400

    Added Cards on home page

--- csvapp/src/components/HomeBody.js
+++ csvapp/src/components/HomeBody.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
+import { Card} from 'react-bootstrap';
 
 class HomeBody extends Component{
     render(){
         return(
             <div>
-                <h1>Hello World</h1>
                 <div id="Mainbody">
                 <br></br><br></br>
                 <center>
-                    <h1>Welcome to CSV Analytics Application</h1>
-                </center>
+                    <h1 id="keepwhite">Welcome to CSV Analytics Application</h1>
+                
                 <br></br><br></br>
+                <div class='parentdiv'>
+                    <div class='childdiv'>
+                <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    <h1>Some quick example text to build on the card title and make up the bulk of the card's content.</h1>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                     </div>
+                     <div class='childdiv'>
+                <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    <h1>Some quick example text to build on the card title and make up the bulk of the card's content.</h1>
+                </Card.Text>
+                </Card.Body>
+                </Card>
+                    </div>
+                </div>
+                </center>
                 {/* TODO: Think of the home content and write this stuff via react-bootstrap */}
                 </div>
             </div>
