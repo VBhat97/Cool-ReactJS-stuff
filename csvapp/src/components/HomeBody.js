cd9d0b39c0fa89d4147e6f9f027aed2de7621ac7
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-07-01 20:21:36 -0400

    Added Home Body with nice looking stuffs

--- csvapp/src/components/HomeBody.js
+++ csvapp/src/components/HomeBody.js
@@ -4,12 +4,14 @@ class HomeBody extends Component{
     render(){
         return(
             <div>
+                <div id="Mainbody">
                 <br></br><br></br>
                 <center>
                     <h1>Welcome to CSV Analytics Application</h1>
                 </center>
-                
+                <br></br><br></br>
                 {/* TODO: Think of the home content and write this stuff via react-bootstrap */}
+                </div>
             </div>
         )
     }
