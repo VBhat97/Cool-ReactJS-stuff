bef03de399b378dc04cf5b4a158d81390acaa70c
Vaibhav Bhat <vaibhav.bhat2097@gmail.com>
2020-07-06 03:49:56 -0400

    Footer changes

--- csvapp/src/components/Footer.js
+++ csvapp/src/components/Footer.js
@@ -7,38 +7,29 @@ class Footer extends Component{
                 <div class="container-fluid">
     <div class="row footer-top">
         <div class="col-sm-4 text-center">
-            <h4 class="ft-text-title">Media Name</h4>
-            <h6 class="ft-desp">Company Name 
-                <br></br>Country Name
+            <h4 class="ft-text-title">Vaibhav Bhat</h4>
+            <h6 class="ft-desp">Lakehead University
+                <br></br>Canada
             </h6>
             <h4 class="details">
                 <a class="contact" href="tel:+977-1-4107223">
-                    <i class="fa fa-phone" aria-hidden="true"></i> +977-000000</a>
+                    <i class="fa fa-phone" aria-hidden="true"></i> +1 807-357-6443</a>
                 </h4>
             </div>
             <div class="col-sm-4 text-center border-left">
-                <h4 class="ft-text-title">Our Team</h4>
+                <h4 class="ft-text-title">Motivation</h4>
                 <div class="address-member">
                     <p class="member">
-                        <b>Director</b> : 
-                    </p>
-                    <p class="member">
-                        <b>Editor</b> : 
-                    </p>
-                    <p class="member">
-                        <b>Reporter</b> : 
-                    </p>
-                    <p class="member">
-                        <b>Reporter</b> : 
+                        <b>To ease the process of adding an initial model for various data analytics methods and to get information about the data</b>
                     </p>
                </div>
            </div>
     </div>
 	<div class="row ft-copyright pt-2 pb-2">
-		<div class="col-sm-4 text-pp-crt">cpoyright 2018 All  Rights Reserved</div>
-		<div class="col-sm-4 text-pp-crt-rg">Department of Information Reg No :</div>
+		<div class="col-sm-4 text-pp-crt"></div>
+		<div class="col-sm-4 text-pp-crt-rg"></div>
 		<div class="col-sm-4 developer">
-			<a href="https://topline-tech.com" class="text-pp-crt">By : <b>T</b>op<b>L</b>ine</a>
+			<a href="https://topline-tech.com" class="text-pp-crt">Copyright 2020 All  Rights Reserved</a>
 		</div>
 	</div>
 </div>
